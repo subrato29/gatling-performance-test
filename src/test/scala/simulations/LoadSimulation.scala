21a905a55671f597bdc577687c48fced8074eb80
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-11 13:35:50 -0400

    load simulation test added

---
+++ src/test/scala/simulations/LoadSimulation.scala
@@ -0,0 +1,35 @@
+package simulations
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class LoadSimulation extends Simulation {
+
+  //Circular, Shuffle, Random, Queue
+  val httpConf = http.baseUrl("https://gorest.co.in")
+    .header("Authorization", "Bearer a67595542e465e56728c1499c323bc2acd1dbbfdbae9e1f53419729e00a1bd38")
+
+  val csvFeeder = csv("./src/test/resources/data/getUser.csv").circular
+
+  def getAllUsers() = {
+    repeat(1) {
+      feed(csvFeeder)
+        .exec(http("Get Single user request")
+          .get("/public/v1/users/${userid}")
+          .check(jsonPath("$.data.name").is("${name}"))
+          .check(status.in(200, 304)))
+        .pause(2)
+    }
+  }
+
+  val scn = scenario("basic load simulation").exec(getAllUsers())
+
+  setUp(scn.inject(nothingFor(5),
+    atOnceUsers(5),
+    rampUsers(10) during(10 seconds)
+    ).protocols(httpConf)
+  )
+}
